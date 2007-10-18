1eb05765717c443ec9613f993a0a2d5b318aba97
antoni.aloy <antoni.aloy@23268ef7-bf3c-0410-ad2c-53259e8ee26d>
2007-10-18 08:24:12 +0000

    First commit. Just to start the project and begin rolling

---
+++ trunk/agenda/urls.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Autor: aaloy
+# -------------------------------------------------------------------
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('agenda.views',
+    # Example:
+     (r'^$', 'index'),
+     (r'^ficha/(?P<accion>\w+)/(?P<id>\d+)/$','ficha'),
+     (r'^edit/(?P<id>\d+)/$','edit'),
+     (r'^add/$','edit'),
+    # Uncomment this for admin:
+)
+
+
+
+
+if __name__ == "__main__":
+    pass
