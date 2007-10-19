382e8628ec3971e9e524379dcc4327db302c15c9
antoni.aloy <antoni.aloy@23268ef7-bf3c-0410-ad2c-53259e8ee26d>
2007-10-19 09:29:42 +0000

    Pagination styles

--- urls.py
+++ urls.py
@@ -1,8 +1,18 @@
 from django.conf.urls.defaults import *
-
+from settings import DEBUG
 urlpatterns = patterns('',
-    # Example:
+     # our agenda application
      (r'^agenda/', include('agenda.urls')),
-    # Uncomment this for admin:
+     # Then admin
      (r'^admin/', include('django.contrib.admin.urls')),
 )
+
+
+# We're going to use the Django server in development, so we'll server
+# also the estatic content.
+if DEBUG:
+	urlpatterns += patterns('',
+       (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'./media/'}),
+    )
+
+
