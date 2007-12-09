b687c10914192c629735b7f8de97c6b85e004c27
antoni.aloy <antoni.aloy@23268ef7-bf3c-0410-ad2c-53259e8ee26d>
2007-12-09 18:14:16 +0000

    Activating the cache, but it does not work

--- settings.py
+++ settings.py
@@ -61,6 +61,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
+#    'django.middleware.cache.CacheMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -68,7 +69,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.doc.XViewMiddleware',
     'django.middleware.gzip.GZipMiddleware',
     # Not using cache
-    #'django.middleware.cache.CacheMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
