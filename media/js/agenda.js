39d2d9e1703cb9568a6529b2433b4e43762364a8
antoni.aloy <antoni.aloy@23268ef7-bf3c-0410-ad2c-53259e8ee26d>
2008-03-06 16:51:03 +0000

    Found the bug. Store object is asyncronous so we need to add a callback funcion to display the data when the load is completed.

--- media/js/agenda.js
+++ media/js/agenda.js
@@ -3,36 +3,72 @@
  */
 
 Ext.onReady(function() {
+
+	   var AgendaRecord = Ext.data.Record.create([        
+                {name: 'first_name', mapping: 'fields.first_name'},
+                {name: 'last_name', mapping: 'fields.last_name'},
+                {name: 'phone',mapping: 'fields.phone'},
+                {name: 'age',mapping: 'fields.age'},
+                {name: 'comments',mapping: 'fields.comments'}
+        ]);
 	
 		var ds = new Ext.data.Store({
 		    proxy: new Ext.data.HttpProxy({url: "/agenda/json/list/"}),
-		    reader: new Ext.data.DjangoJsonReader({
+		    reader: new Ext.data.JsonReader({
 		        root: 'rows',
-		        totalProperty: 'total'
-		    }, [
-		        {name: 'first_name'},
-		        {name: 'last_name'},
-				{name: 'phone'},
-				{name: 'age'},
-				{name: 'comments'}
-		    ])
+		        totalProperty: 'total',
+				id: 'pk'
+		    }, [        
+                {name: 'first_name', mapping: 'fields.first_name'},
+                {name: 'last_name', mapping: 'fields.last_name'},
+                {name: 'phone',mapping: 'fields.phone'},
+                {name: 'age',mapping: 'fields.age'},
+                {name: 'comments',mapping: 'fields.comments'}
+                ]
+			)
 		});  
 
-        ds.load();		
-        var grid = new Ext.grid.GridPanel({
-                store: ds,				
-                columns: [
-                        {header: 'First Name', width: 120, sortable: true, dataIndex: 'first_name'},
-                        {header: 'Last Name', width: 120, sortable: true, dataIndex: 'last_name'},
-                ],                
-                viewConfig: {
-                        forceFit: true
-                },
-                renderTo: 'content',
-                title: 'Agenda list',
-                width: 500,
-                frame: true
-        });
-
-        grid.getSelectionModel().selectFirstRow();
+        function datos_cargados(){		
+			var grid = new Ext.grid.GridPanel({
+				store: ds,
+				columns: [{
+					header: 'First Name',
+					width: 120,
+					sortable: true,
+					dataIndex: 'first_name'
+				}, {
+					header: 'Last Name',
+					width: 120,
+					sortable: true,
+					dataIndex: 'last_name'
+				}, {
+					header: 'Phone',
+					width: 60,
+					sortable: true,
+					dataIndex: 'phone'
+				}, {
+					header: 'Age',
+					width: 60,
+					sortable: true,
+					dataIndex: 'age'
+				}, {
+					header: 'Comments',
+					width: 60,
+					sortable: true,
+					dataIndex: 'comments'
+				}, ],
+				viewConfig: {
+					forceFit: true
+				},
+				renderTo: 'content',
+				title: 'Agenda list',
+				width: 500,
+				frame: true,
+				loadMask: true,
+				sm: new Ext.grid.RowSelectionModel({singleSelect:true}),
+			});
+			grid.show();
+			grid.getSelectionModel().selectFirstRow();
+		}
+        ds.load({callback:datos_cargados});
 });
