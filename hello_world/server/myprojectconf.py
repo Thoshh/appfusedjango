6fdef89b7eaf062acbb3f536cd6cc4255fe6f263
antoni.aloy <antoni.aloy@23268ef7-bf3c-0410-ad2c-53259e8ee26d>
2009-05-10 16:35:50 +0000

    Comparing with PHP
    http://wiki.kumbiaphp.com/images/Benchmark.png

---
+++ hello_world/server/myprojectconf.py
@@ -0,0 +1,40 @@
+###########################################################################
+# Configuration
+#
+# Django settings
+DJANGO_SETTINGS = 'settings'
+DJANGO_SERVE_ADMIN = False # Serve admin files
+
+# Server settings
+IP_ADDRESS = '127.0.0.1'
+PORT = 8088
+SERVER_NAME = 'localhost'
+SERVER_THREADS = 5
+# Change it to True if you want it to run as daemon, if you use a
+# daemon.sh file you should also change it to True
+RUN_AS_DAEMON = False
+DAEMON_RUN_DIR = '/' # The daemon will change directory to this one
+                     # this is needed to prevent unmounting your
+                     # disk.
+
+# Log settings
+LOGFILE = 'webserver.log'
+LOGLEVEL = 'INFO' # if DEBUG is True, overwritten to DEBUG
+DEBUG = False
+
+# It must match with the path given in your daemon.sh file if you are
+# using a daemon.sh file to control the server.
+PIDFILE = '/tmp/django-myproject.pid'
+
+# Launch as root to dynamically chown
+SERVER_USER = 'nobody'
+SERVER_GROUP = 'nobody'
+
+# Enable SSL, if enabled, the certificate and private key must
+# be provided.
+SSL = False
+SSL_CERTIFICATE = '/full/path/to/certificate'
+SSL_PRIVATE_KEY = '/full/path/to/private_key'
+
+#
+###########################################################################
