e90afbdb19ac56efc2f5592413c738d6284e5582
antoni.aloy <antoni.aloy@23268ef7-bf3c-0410-ad2c-53259e8ee26d>
2010-01-20 23:51:49 +0000

    testeando la lista de espera de inscripciones

--- prj_events_registration/evento/tests/__init__.py
+++ prj_events_registration/evento/tests/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-# Antoni Aloy (antoni.aloy@trespams.com)
+# Antoni Aloy (antoni.aloy{at}trespams.com)
 
 from evento.tests.test_evento_views import *
-
+from evento.tests.test_evento_signals import *
 
